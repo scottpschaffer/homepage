c4754ed986e236242179f8b78afec8e21e3436ce
scottpschaffer <scottpschaffer@yahoo.com>
2017-01-06 21:26:16 -0600

    Added IFrame stuff and coded About and Projects pages.

---
+++ work.js
@@ -0,0 +1,22 @@
+function onclick1 (){
+    $('.nav.navbar-nav > li').on('click', function(e) {
+    $('.nav.navbar-nav > li').removeClass('active');
+    $(this).addClass('active');
+}); 
+}
+
+function onclick2(){
+    $('.nav.navbar-nav > li').on('click', function(e) {
+    $('.nav.navbar-nav > li').removeClass('active');
+    $(this).addClass('active');
+}); 
+}
+
+
+function onclick3(){
+    $('.nav.navbar-nav > li').on('click', function(e) {
+    $('.nav.navbar-nav > li').removeClass('active');
+    $(this).addClass('active');
+}); 
+}
+
